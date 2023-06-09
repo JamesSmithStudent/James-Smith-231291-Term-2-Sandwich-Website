d150fc33b99526ea693da6618797650716240b44
JamesSmithStudent <125651123+JamesSmithStudent@users.noreply.github.com>
2023-06-09 18:09:46 +0200

    2ncd Milestone

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 addPrice = () => {
-    let white = 10
-    let brown = 8
-    let fiber = 5000
+    let finalprice = 0 ;
+    let white = document.getElementById("white").checked;
+    let brown = document.getElementById("brown").checked;
+    let fiber = document.getElementById("fiber").checked;
 
-
+    
+    if (white){
+        finalprice = finalprice + 10 ;
+        alert('Your sandwich is R'+finalprice+".00")
+    }
+    if (brown){
+        finalprice = finalprice + 8 ;
+        alert('Your sandwich is R'+finalprice+".00")
+    }
+    if (fiber){
+        finalprice = finalprice + 10000 ;
+        alert('Your sandwich is R'+finalprice+".00")
+    }
     
 }
