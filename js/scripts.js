1b2a2160f80ddf572f1ec08e8539ec85ba119a0d
JamesSmithStudent <125651123+JamesSmithStudent@users.noreply.github.com>
2023-06-17 20:52:00 +0200

    another commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,47 @@
-addPrice = () => {
-    let finalprice = 0 ;
-    let white = document.getElementById("white").checked;
-    let brown = document.getElementById("brown").checked;
-    let fiber = document.getElementById("fiber").checked;
+addSandwich = () => {
 
-    
-    if (white){
-        finalprice = finalprice + 10 ;
-        alert('Your sandwich is R'+finalprice+".00")
+    let priceTotal = 0;
+
+
+    let breadOptions = document.getElementsByName("bread");
+    let breadValue;
+    for(let i = 0; i < breadOptions.length; i++){
+       if(breadOptions[i].checked){
+        breadValue = breadOptions[i].value
+        priceTotal = priceTotal + +breadOptions[i].dataset.cost
+       }
     }
-    if (brown){
-        finalprice = finalprice + 8 ;
-        alert('Your sandwich is R'+finalprice+".00")
+
+
+
+    let meatOptions = document.getElementsByName("meat");
+    let meatChoice = [];
+    for(let i = 0; i < meatOptions.length; i++){
+       if(meatOptions[i].checked){
+        meatChoice.push(meatOptions[i].value)
+        priceTotal = priceTotal + +meatOptions[i].dataset.cost
+       }
     }
-    if (fiber){
-        finalprice = finalprice + 10000 ;
-        alert('Your sandwich is R'+finalprice+".00")
+
+
+    let ekstraOptions = document.getElementsByName("ekstra");
+    let ekstraChoice = [];
+    for(let i = 0; i < ekstraOptions.length; i++){
+       if(ekstraOptions[i].checked){
+        ekstraChoice.push(ekstraOptions[i].value)
+        priceTotal = priceTotal + +ekstraOptions[i].dataset.cost
+       }
     }
-    
+
+    let sauceOptions = document.getElementsByName("sauce");
+    let sauceChoice = [];
+    for(let i = 0; i < sauceOptions.length; i++){
+       if(sauceOptions[i].checked){
+         sauceChoice.push(sauceOptions[i].value)
+        priceTotal = priceTotal + +sauceOptions[i].dataset.cost
+       }
+    }
+  
+console.log(priceTotal);
 }
+
